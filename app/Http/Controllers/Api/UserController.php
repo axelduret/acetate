4c479c633a958a4eeb4f4d0c86d2182d7125811e
Axel Duret <axelfaya@gmail.com>
2021-04-29 12:07:02 +0200

    add index method in UserController

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -2,63 +2,103 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\UserCollection;
+use App\Models\User;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
+  // Default pagination value.
+  const PER_PAGE = 50;
+
+  /**
+   * Sortable fields.
+   *
+   * @var array
+   */
+  protected $sortFields = ['id', 'username', 'email', 'firstname', 'lastname', 'company', 'language'];
+
+  /**
+   * Searchable fields.
+   *
+   * @var array
+   */
+  protected $searchFields = ['username', 'email', 'firstname', 'lastname', 'company', 'language'];
+
+  /**
+   * Display a list of users.
+   *
+   * @param Request $request
+   * @return Response
+   */
+  public function index(Request $request)
+  {
+    // Retrieve the data and sort it.
+    $sortField = $request->input('sort_by');
+    $sortOrder = $request->input('order_by') ?? 'asc';
+    $sortableFields = in_array($sortField, $this->sortFields) ? $sortField : 'id';
+    $query = User::orderBy($sortableFields, $sortOrder);
+
+    // Search data.
+    $searchField = $request->input('search_field');
+    $searchValue = $request->input('search_value');
+    $searchableFields = in_array($searchField, $this->searchFields) ? $searchField : null;
+    if (!is_null($searchValue) && !is_null($searchableFields)) {
+      $searchQuery = "%$searchValue%";
+      $query = $query->where($searchableFields, 'like', $searchQuery);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+    // Pagination.
+    $perPage = $request->input('per_page') ?? self::PER_PAGE;
+    $users = $query->paginate((int)$perPage);
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+    // Format data.
+    return new UserCollection($users);
+  }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+    //
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+  /**
+   * Display the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function show($id)
+  {
+    //
+  }
+
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, $id)
+  {
+    //
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy($id)
+  {
+    //
+  }
 }
---
+++ app/Http/Resources/PersonCollection.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class PersonCollection extends ResourceCollection
+{
+  /**
+   * Transform the resource collection into an array.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return array
+   */
+  public function toArray($request)
+  {
+    return [
+      $this::$wrap => $this->collection,
+    ];
+  }
+
+  /**
+   * The "data" wrapper that should be applied.
+   *
+   * @var string
+   */
+  public static $wrap = 'people';
+}
