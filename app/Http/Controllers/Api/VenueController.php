682acd8dab5a497c5c40358963d1a0eee9ea70ff
Axel Duret <axelfaya@gmail.com>
2021-04-29 11:33:21 +0200

    edit Controllers

--- app/Http/Controllers/Api/VenueController.php
+++ app/Http/Controllers/Api/VenueController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class Venue extends Controller
+class VenueController extends Controller
 {
     /**
      * Display a listing of the resource.
