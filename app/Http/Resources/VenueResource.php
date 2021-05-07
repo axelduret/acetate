28b9999d0762e26c0846c736d9df994b5ebf4268
Axel Duret <axelfaya@gmail.com>
2021-05-07 23:34:27 +0200

    add likes/dislikes count to Comments

--- app/Http/Resources/VenueResource.php
+++ app/Http/Resources/VenueResource.php
@@ -36,6 +36,10 @@ class VenueResource extends JsonResource
             'created_at' => date('Y-m-d H:i:s', strtotime($item['created_at'])),
             'updated_at' => date('Y-m-d H:i:s', strtotime($item['updated_at'])),
             'text' => $item['text'],
+            // TODO show likes of the logged user.
+            'likes_count' => $item->likes->where('is_dislike', 0)->count(),
+            // TODO show dislikes of the logged user.
+            'dislikes_count' => $item->likes->where('is_dislike', 1)->count(),
             'user_id' => $item['user_id'],
           ];
         }),
