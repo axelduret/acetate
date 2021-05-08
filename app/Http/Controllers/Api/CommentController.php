cf8d0b438e3c35bc71daee9b8ea4e5e27c09e738
Axel Duret <axelfaya@gmail.com>
2021-05-08 14:27:53 +0200

    add index method in CommentController

--- app/Http/Controllers/Api/CommentController.php
+++ app/Http/Controllers/Api/CommentController.php
@@ -15,6 +15,9 @@ class CommentController extends Controller
   // Import custom response trait.
   use RespondsWithHttpStatus;
 
+  // Pagination.
+  const PER_PAGE = 50;
+
   /**
    * Response messages.
    *
@@ -30,13 +33,82 @@ class CommentController extends Controller
   protected $warning = [];
 
   /**
+   * Sortable fields.
+   *
+   * @var array
+   */
+  protected $sortFields = ['created_at', 'updated_at', 'likes_count', 'dislikes_count', 'user_id'];
+
+  /**
    * Display the list of all comments.
    *
+   * @param  Request  $request
    * @return CommentCollection
    */
-  public function index()
+  public function index(Request $request)
   {
-    //
+    // Retrieve data with requested relationships.
+    $query = Comment::
+      // Returns the total number of likes.
+      // TODO show likes of the logged user.
+      withCount([
+        'likes as likes_count' => function ($filter) {
+          $filter
+            ->where('is_dislike', 0);
+        },
+        // Returns the total number of dislikes.
+        // TODO show dislikes of the logged user.
+        'likes as dislikes_count' => function ($filter) {
+          $filter
+            ->where('is_dislike', 1);
+        }
+      ])
+      ->with([
+        // Returns event:id and event:name.
+        'event' => function ($filter) {
+          $filter
+            ->select('id', 'name')
+            // Order events by name.
+            ->orderBy('name');
+        },
+        // Returns people:id and people:nickname.
+        'person' => function ($filter) {
+          $filter
+            ->select('id', 'nickname')
+            // Order people by nickname.
+            ->orderBy('nickname');
+        },
+        // Returns venues:id and venues:name.
+        'venue' => function ($filter) {
+          $filter
+            ->select('id', 'name')
+            // Order venues by name.
+            ->orderBy('name');
+        }
+      ]);
+    // Sort data.
+    $sortField = in_array(
+      $request->input('sort_by'),
+      $this->sortFields
+    ) // Accepted values.
+      ? $request->input('sort_by') // Submitted value.
+      : 'created_at'; // Default value.
+    $sortOrder = in_array(
+      $request->input('order_by'),
+      ['asc', 'desc']
+    ) // Accepted values.
+      ? $request->input('order_by') // Submitted value.
+      : 'asc'; // Default value.
+    $query = $query->orderBy($sortField, $sortOrder);
+    // Pagination.
+    $perPage = $request->input('per_page') ?? self::PER_PAGE;
+    $comments = $query->paginate((int)$perPage);
+    // Check if users exist.
+    if ($comments == '') {
+      return $this->failure('No comments were found.', 404);
+    }
+    // Format data.
+    return new CommentCollection($comments);
   }
 
   /**
--- app/Http/Resources/CommentCollection.php
+++ app/Http/Resources/CommentCollection.php
@@ -14,7 +14,49 @@ class CommentCollection extends ResourceCollection
    */
   public function toArray($request)
   {
-    return parent::toArray($request);
+    return [
+      $this::$wrap => $this->collection->map(function ($item) {
+        $person = null;
+        if ($item->person) {
+          $person = [
+            'id' => $item->person['id'],
+            'nickname' => $item->person['nickname'],
+          ];
+        }
+        $event = null;
+        if ($item->event) {
+          $event = [
+            'id' => $item->event['id'],
+            'name' => $item->event['name'],
+          ];
+        }
+        $venue = null;
+        if ($item->venue) {
+          $venue = [
+            'id' => $item->venue['id'],
+            'name' => $item->venue['name'],
+          ];
+        }
+        return collect([
+          'id' => $item->id,
+          'created_at' => date('Y-m-d H:i:s', strtotime($item->created_at)),
+          'updated_at' => date('Y-m-d H:i:s', strtotime($item->updated_at)),
+          'text' => $item->text,
+          'event' => $item->event,
+          'person' => $item->person,
+          'venue' => $item->venue,
+          // TODO show likes of the logged user.
+          'likes_count' => $item->likes_count,
+          // TODO show dislikes of the logged user.
+          'dislikes_count' => $item->dislikes_count,
+          'user_id' => $item->user_id,
+        ]);
+      }),
+      'credit' => env('APP_CREDIT'),
+      'website' => env('APP_URL'),
+      'licence' => env('APP_LICENCE'),
+      'timezone' => env('TIME_ZONE'),
+    ];
   }
 
   /**
