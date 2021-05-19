c281432e13cc35505d730ca281d8b89df75f4e22
Axel Duret <axelfaya@gmail.com>
2021-05-19 20:38:51 +0200

    edit Vue props to accept uuid

--- public/js/resources_js_components_pages_Event_vue.js
+++ public/js/resources_js_components_pages_Event_vue.js
@@ -66,7 +66,7 @@ __webpack_require__.r(__webpack_exports__);
 //
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
   props: {
-    Id: Number,
+    Id: String,
     Avatar: String
   },
   data: function data() {
