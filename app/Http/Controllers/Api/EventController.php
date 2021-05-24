6a57f150c9be45a4e42c43d28d6bcfa08724f7a2
Axel Duret <axelfaya@gmail.com>
2021-05-24 14:24:11 +0200

    edit EventController

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -110,9 +110,9 @@ class EventController extends Controller
       ? Carbon::parse($request->input('search_reference'))->toDateString()
       : Carbon::now()->toDateString();
     // By default, only returns events where date is superior or equal to today.
-    $query = Event::with('taxonomies')->whereHas('taxonomies', function ($filter) {
+    $query = Event::with('taxonomies')/* ->whereHas('taxonomies', function ($filter) {
       $filter->where('type', '=', 'music');
-    })->get();
+    })->get() */;
     /* // Returns the list of dates with attached relationships.
       ->with([
         // Returns event:id, event:name and event:avatar.
